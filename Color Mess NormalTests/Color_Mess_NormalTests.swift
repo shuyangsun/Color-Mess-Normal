5969c95206a3325428c664de2880087c60d34d46
Shuyang Sun <sunbuffett@gmail.com>
2015-11-04 16:39:32 -0600

    Initial Commit

---
+++ Color Mess NormalTests/Color_Mess_NormalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Color_Mess_NormalTests.swift
+//  Color Mess NormalTests
+//
+//  Created by Shuyang Sun on 11/4/15.
+//  Copyright © 2015 Shuyang Sun. All rights reserved.
+//
+
+import XCTest
+@testable import Color_Mess_Normal
+
+class Color_Mess_NormalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
